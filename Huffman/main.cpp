a8aac69e16cef849998f564ac1494f9b5586bbd7
lotu <gao527950554@foxmail.com>
2018-03-18 21:24:27 +0800

    Initial Commit

---
+++ Huffman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Huffman
+//
+//  Created by lotu on 2018/3/18.
+//  Copyright © 2018年 lotu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
